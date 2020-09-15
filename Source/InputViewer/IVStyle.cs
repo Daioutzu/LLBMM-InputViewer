e407161ba82ef20c762f8b9f87faed5d19d12aef
Daioutzu <54756730+Daioutzu@users.noreply.github.com>
2020-09-15 19:13:11 +0100

    Added Source Code + The Resources for InputViewer.

---
+++ Source/InputViewer/IVStyle.cs
@@ -0,0 +1,414 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace InputViewer
+{
+    public static class IVStyle
+    {
+
+        private static string resourceFolder = Application.dataPath + "/Managed/InputViewerResources";
+        private static string UiFolder = "assets/ui/texture2D/";
+
+        public static AssetBundle uiBundle;
+        public static Font inputViewerFont;
+        public static Dictionary<string, Texture2D> uiAssets = new Dictionary<string, Texture2D>();
+
+        static void LoadAssets()
+        {
+            uiBundle = AssetBundle.LoadFromFile(resourceFolder + "/Bundles/UI");
+            foreach (var assetPathAndName in uiBundle.GetAllAssetNames())
+            {
+                var name = Path.GetFileNameWithoutExtension(assetPathAndName);
+                var nameExt = Path.GetFileName(assetPathAndName);
+                uiAssets.Add(name, uiBundle.LoadAsset<Texture2D>(UiFolder + nameExt));
+            }
+            inputViewerFont = uiBundle.LoadAsset<Font>("assets/ui/elements.ttf");
+        }
+
+        public static void ATInit()
+        {
+            LoadAssets();
+        }
+
+        public static GUIStyle inputViewerBG
+        {
+            get
+            {
+                GUIStyleState bg = new GUIStyleState();
+                bg.background = uiAssets["viewerbg"];
+                bg.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle()
+                {
+                    font = inputViewerFont,
+                    padding = new RectOffset(4, 4, 4, 4),
+                    normal = bg,
+                    onNormal = bg,
+                    hover = bg,
+                    onHover = bg,
+                    active = bg,
+                    onActive = bg,
+                };
+                return gUIStyle;
+            }
+        }
+
+        static int combatKeySize = 30;
+        static RectOffset btnMargin = new RectOffset(-1, -1, -1, -1);
+
+        public static GUIStyle combatBtnStyle
+        {
+            get
+            {
+                GUIStyle gUIStyle = new GUIStyle(GUI.skin.label)
+                {
+                    fontSize = 20,
+                    alignment = TextAnchor.MiddleCenter,
+                    fontStyle = FontStyle.Bold,
+                    padding = new RectOffset(4, 4, 4, 4),
+                    margin = btnMargin,
+                    fixedHeight = combatKeySize,
+                    fixedWidth = combatKeySize,
+                };
+                return gUIStyle;
+            }
+        }
+
+        public static GUIStyle swingStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["swingoff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["swingon"];
+                on.textColor = Color.black;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+
+        public static GUIStyle buntStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["buntoff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["bunton"];
+                on.textColor = Color.black;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle grabStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["graboff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["grabon"];
+                on.textColor = Color.black;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle tauntStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["tauntoff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["taunton"];
+                on.textColor = Color.black;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle jumpStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["jumpoff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["jumpon"];
+                on.textColor = Color.black;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle dirUpStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["arrowuoff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["arrowuon"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+
+        public static GUIStyle dirDwnStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["arrowdoff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["arrowdon"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle dirLefStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["arrowloff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["arrowlon"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle dirRigStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["arrowroff"];
+                off.textColor = Color.black;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["arrowron"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(combatBtnStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle expressStyle
+        {
+            get
+            {
+                GUIStyle gUIStyle = new GUIStyle(GUI.skin.label)
+                {
+                    fontSize = 14,
+                    alignment = TextAnchor.MiddleCenter,
+                    fontStyle = FontStyle.Bold,
+                    padding = new RectOffset(0, 0, 0, 0),
+                    margin = new RectOffset(0, 0, 0, 0),
+                    fixedWidth = 68,
+                    fixedHeight = 34,
+                };
+                return gUIStyle;
+            }
+        }
+
+        public static GUIStyle expNiceStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["niceoff"];
+                off.textColor = Color.white;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["niceon"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(expressStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+
+        public static GUIStyle expOopsStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["oopsoff"];
+                off.textColor = Color.white;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["oopson"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(expressStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+        public static GUIStyle expWowStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["wowoff"];
+                off.textColor = Color.white;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["wowon"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(expressStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+
+        public static GUIStyle expBringItStyle
+        {
+            get
+            {
+                GUIStyleState off = new GUIStyleState();
+                off.background = uiAssets["bringitoff"];
+                off.textColor = Color.white;
+
+                GUIStyleState on = new GUIStyleState();
+                on.background = uiAssets["bringiton"];
+                on.textColor = Color.white;
+
+                GUIStyle gUIStyle = new GUIStyle(expressStyle)
+                {
+                    normal = off,
+                    hover = off,
+                    active = off,
+                    onNormal = on,
+                    onHover = on,
+                    onActive = on,
+                };
+                return gUIStyle;
+            }
+        }
+
+    }
+}
---
+++ Source/InputViewer/InputViewer.cs
@@ -0,0 +1,247 @@
+﻿using LLHandlers;
+using LLScreen;
+using UnityEngine;
+
+namespace InputViewer
+{
+    class InputViewer : MonoBehaviour
+    {
+
+        private const string modVersion = "1.0a";
+        private const string repositoryOwner = "Daioutzu";
+        private const string repositoryName = "LLBMM-InputViewer";
+
+        public static InputViewer Instance { get; private set; } = null;
+        public static ModMenuIntegration MMI = null;
+        private bool modIntegrated;
+        public static void Initialize()
+        {
+            GameObject gameObject = new GameObject("InputViewer"); //The game object is what we use to interact with our mod
+            Instance = gameObject.AddComponent<InputViewer>();
+            DontDestroyOnLoad(gameObject); // Makes sure our game object isn't destroyed
+            IVStyle.ATInit();
+        }
+
+        private void Start()
+        {
+            if (MMI == null) { MMI = gameObject.AddComponent<ModMenuIntegration>(); Debug.Log("[LLBMM] InputViewer: Added GameObject \"ModMenuIntegration\""); }
+            Debug.Log("[LLBMM] InputViewer Started");
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("[LLBMM] InputViewer Destroyed");
+        }
+
+        bool InGame => World.instance != null && (GetCurrentGameState() == JOFJHDJHJGI.CDOFDJMLGLO || GetCurrentGameState() == JOFJHDJHJGI.LGILIJKMKOD) && !UIScreen.loadingScreenActive;
+        float selectViewingMode = 4;
+        bool showInLobby = false;
+
+        private void ModMenuInit()
+        {
+            if ((MMI != null && !modIntegrated) || LLModMenu.ModMenu.Instance.currentOpenMod == "InputViewer")
+            {
+                selectViewingMode = MMI.GetSliderValue("(slider)selectViewingMode");
+                showInLobby = MMI.GetTrueFalse(MMI.configBools["(bool)showInLobby"]);
+                if (!modIntegrated) { Debug.Log("[LLBMM] InputViewer: ModMenuIntegration Done"); };
+                modIntegrated = true;
+            }
+        }
+        void Update()
+        {
+            ModMenuInit();
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                Cursor.visible = !Cursor.visible;
+            }
+        }
+
+        Rect inputRect = new Rect(30, Screen.height - 147, 300, 117);
+
+        void OnGUI()
+        {
+            if (ViewingMode((ViewMode)selectViewingMode))
+            {
+                inputRect = GUI.Window(102289, inputRect, InputWindow, "", IVStyle.inputViewerBG);
+            }
+        }
+
+        JOFJHDJHJGI GetCurrentGameState()
+        {
+            return DNPFJHMAIBP.HHMOGKIMBNM();
+        }
+
+        GameMode GetCurrentGameMode()
+        {
+            return JOMBNFKIHIC.GIGAKBJGFDI.PNJOKAICMNN;
+        }
+
+        bool isOnline()
+        {
+            return JOMBNFKIHIC.GDNFJCCCKDM;
+        }
+
+        enum ViewMode
+        {
+            Off,
+            Training,
+            local,
+            Online,
+            All,
+        }
+
+        enum GameState
+        {
+            LOBBY_STORY = 23,
+            LOBBY_TUTORIAL = 12,
+            LOBBY_TRAINING = 11,
+            LOBBY_CHALLENGE = 6,
+            LOBBY_LOCAL = 4,
+            GAME = 19,
+            GAME_PAUSE = 20,
+            LOBBY_ONLINE = 5,
+        }
+
+        bool LocalLobby()
+        {
+            if (!showInLobby) return false;
+
+            switch (GetCurrentGameState())
+            {
+                case (JOFJHDJHJGI)GameState.LOBBY_LOCAL:
+                case (JOFJHDJHJGI)GameState.LOBBY_TRAINING:
+                case (JOFJHDJHJGI)GameState.LOBBY_CHALLENGE:
+                case (JOFJHDJHJGI)GameState.LOBBY_TUTORIAL:
+                case (JOFJHDJHJGI)GameState.LOBBY_STORY:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        bool OnlineLobby()
+        {
+            if (!showInLobby) return false;
+
+            switch (GetCurrentGameState())
+            {
+                case (JOFJHDJHJGI)GameState.LOBBY_ONLINE:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        bool ViewingMode(ViewMode selectedView)
+        {
+            switch (selectedView)
+            {
+                case ViewMode.Off:
+                    return false;
+                case ViewMode.Training:
+                    return GetCurrentGameMode() == GameMode.TRAINING && (InGame || (showInLobby && GetCurrentGameState() == (JOFJHDJHJGI)GameState.LOBBY_TRAINING));
+                case ViewMode.local:
+                    return !isOnline() && (InGame || LocalLobby());
+                case ViewMode.Online:
+                    return isOnline() && (InGame || OnlineLobby());
+                case ViewMode.All:
+                    return InGame || LocalLobby() || OnlineLobby();
+                default:
+                    return false;
+            }
+        }
+
+        void InputWindow(int wId)
+        {
+            int index = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                if (ALDOKEMAOMB.BJDPHEHJJJK(i).GAFCIHKIGNM && ALDOKEMAOMB.BJDPHEHJJJK(i).NGLDMOLLPLK)
+                {
+                    index = i;
+                    i = 4;
+                }
+            };
+            ALDOKEMAOMB player = ALDOKEMAOMB.BJDPHEHJJJK(index);
+
+            GUIStyle headerStyle = new GUIStyle(GUI.skin.label)
+            {
+                font = IVStyle.inputViewerFont,
+                fontSize = 20,
+                alignment = TextAnchor.MiddleLeft,
+                margin = new RectOffset(10, 0, 3, 14),
+                wordWrap = false,
+            };
+
+            GUIStyle border = new GUIStyle()
+            {
+                padding = new RectOffset(4, 4, 0, 0),
+            };
+
+            GUIStyle expressBorder = new GUIStyle()
+            {
+                padding = new RectOffset(8, 8, 8, 8),
+                fixedWidth = 150,
+            };
+
+            GUI.DragWindow();
+            GUILayout.BeginArea(new Rect(0, 0, inputRect.width, inputRect.height));
+            GUILayout.BeginHorizontal(border);
+
+            GUILayout.BeginVertical();
+            GUILayout.Label("Input Viewer", headerStyle);
+            GUILayout.BeginHorizontal();
+
+            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.JUMP)] == 100 ? true : false, "", IVStyle.jumpStyle);
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.UP)] == 100 ? true : false, "", IVStyle.dirUpStyle);
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.LEFT)] == 100 ? true : false, "", IVStyle.dirLefStyle);
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.DOWN)] == 100 ? true : false, "", IVStyle.dirDwnStyle);
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.RIGHT)] == 100 ? true : false, "", IVStyle.dirRigStyle);
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+
+            GUILayout.BeginVertical();
+            GUILayout.BeginHorizontal();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.SWING)] == 100 ? true : false, "", IVStyle.swingStyle); //Swing Button
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.BUNT)] == 100 ? true : false, "", IVStyle.buntStyle); //Bunt Button
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.GRAB)] == 100 ? true : false, "", IVStyle.grabStyle); //Grab Button
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.TAUNT)] == 100 ? true : false, "", IVStyle.tauntStyle); //Taunt Button
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+            GUILayout.FlexibleSpace();
+
+            GUILayout.BeginVertical(expressBorder);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.EXPRESS_UP)] == 100 ? true : false, "", IVStyle.expNiceStyle);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.EXPRESS_LEFT)] == 100 ? true : false, "", IVStyle.expOopsStyle);
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.EXPRESS_RIGHT)] == 100 ? true : false, "", IVStyle.expWowStyle);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Toggle(InputHandler.currentInput[player.CJFLMDNNMIE, InputAction.ActionToIndex(InputAction.EXPRESS_DOWN)] == 100 ? true : false, "", IVStyle.expBringItStyle);
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
+        }
+
+    }
+}
